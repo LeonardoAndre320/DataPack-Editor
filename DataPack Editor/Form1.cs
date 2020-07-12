48954077ea86c78a2a4800dc6e8e53a17ff14a88
Leonardo <leonardoandre320@gmail.com>
2020-07-12 11:26:32 -0300

    Tela de carregamento. front

--- DataPack Editor/Form1.cs
+++ DataPack Editor/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace DataPack_Editor
 {
-    public partial class Form1 : Form
+    public partial class Carregar : Form
     {
-        public Form1()
+        public Carregar()
         {
             InitializeComponent();
         }
